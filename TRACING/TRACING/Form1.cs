e7e9403cf67da43395cb2eb66804d460f65af494
hrewhwreh <pavelvedrukov@mail.ru>
2020-05-18 14:05:07 +0300

    com lab

---
+++ TRACING/TRACING/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace TRACING
+{
+    public partial class Form1 : Form
+    {
+        int BasicProgramID;
+        int BasicVertexShader;
+        int BasicFragmentShader;
+
+        void loadShader(String filename, ShaderType type, int program, out int address)
+        {
+            address = GL.CreateShader(type);
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+            GL.AttachShader(program, address);
+            Console.WriteLine(GL.GetShaderInfoLog(address));
+        }
+
+        void InitShaders()
+        {
+            BasicProgramID = GL.CreateProgram();
+            loadShader("..\\..\\Shaders\\raytracing.vert", ShaderType.VertexShader, BasicProgramID, out BasicVertexShader);
+            loadShader("..\\..\\Shaders\\raytracing.frag", ShaderType.FragmentShader, BasicProgramID, out BasicFragmentShader);
+            GL.LinkProgram(BasicProgramID);
+            int status = 0;
+            GL.GetProgram(BasicProgramID, GetProgramParameterName.LinkStatus, out status);
+            Console.WriteLine(GL.GetProgramInfoLog(BasicProgramID));
+        }
+
+        private static bool Init()
+        {
+            GL.Enable(EnableCap.ColorMaterial);
+            GL.ShadeModel(ShadingModel.Smooth);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.Enable(EnableCap.CullFace);
+
+            GL.Enable(EnableCap.Lighting);
+            GL.Enable(EnableCap.Light0);
+
+            GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+
+            return true;
+        }
+
+        int vbo_position;
+        int attribute_vpos = 1;
+
+        void Draw()
+        {
+            GL.ClearColor(Color.AliceBlue);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            ;
+            GL.UseProgram(BasicProgramID);
+            // Quad
+            GL.Color3(Color.White);
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.TexCoord2(0, 1);
+            GL.Vertex2(-1, -1);
+
+            GL.TexCoord2(1, 1);
+            GL.Vertex2(1, -1);
+
+            GL.TexCoord2(1, 0);
+            GL.Vertex2(1, 1);
+
+            GL.TexCoord2(0, 0);
+            GL.Vertex2(-1, 1);
+
+            GL.End();
+            glControl1.SwapBuffers();
+            GL.UseProgram(0);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            InitShaders();
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            Draw();
+        }
+    }
+}
+
